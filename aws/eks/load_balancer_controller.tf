f36d544bd3967c7ae868e7615e3d7688f19d04ed
Dave Barnum <DAVE_BARNUM@wctnet.org>
2023-05-12 15:53:35 -0400

    EKS Module: Add support for load-balancer-controller and cert-manager-controller module.

---
+++ aws/eks/load_balancer_controller.tf
@@ -0,0 +1,17 @@
+/*
+ * Install the Cert Manager Controller.
+ */
+module "eks_load_balancer_controller" {
+  count    = var.enable_load_balancer_controller ? 1: 0
+
+  app_name = var.app_name
+  app_env  = var.app_env
+  source = "github.com/Wycliffe-USA/terraform-modules?ref=1.4.0/aws/eks_load_balancer_controller"
+
+
+  chart_version             = var.load_balancer_controller_helm_chart_version
+  cluster_name              = module.eks.cluster_name
+  aws_iam_oidc_provider_arn = module.eks.oidc_provider_arn
+
+  tags = local.tags
+}
