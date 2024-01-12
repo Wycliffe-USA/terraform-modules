e8185df6531164c5cf7a6a3eaaaa056e0731afb6
Dave Barnum <DAVE_BARNUM@wctnet.org>
2024-01-12 13:46:33 -0500

    Add Cloudflare-sg module.

---
+++ aws/cloudflare-sg/main.tf
@@ -0,0 +1,18 @@
+module "cf_ips" {
+  source = "github.com/wycliffe-usa/terraform-modules//cloudflare/ips?ref=1.7.1"
+}
+
+resource "aws_security_group" "cloudflare_https" {
+  name        = "cloudflare-https"
+  description = "Allow HTTPS traffic from Cloudflare"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group_rule" "cloudflare_ipv4" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.cloudflare_https.id
+  cidr_blocks       = module.cf_ips.ipv4_cidrs
+}
