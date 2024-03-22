b8856b3bbf81c79c0ee398c63dbcea6660cffbba
Dave Barnum <DAVE_BARNUM@wctnet.org>
2024-03-22 16:47:14 -0400

    Update cloudflare IPs to use cloudflare data object.

--- cloudflare/ips/main.tf
+++ cloudflare/ips/main.tf
@@ -1,7 +1,1 @@
-data "http" "cloudflare_ipv4" {
-  url = "https://www.cloudflare.com/ips-v4"
-}
-
-data "http" "cloudflare_ipv6" {
-  url = "https://www.cloudflare.com/ips-v6"
-}
+data "cloudflare_ip_ranges" "cloudflare" {}
